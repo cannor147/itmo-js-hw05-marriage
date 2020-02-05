bbf824f0ea5dae8792cebae90caa190b2d927c93
cannor147 <cannor147@gmail.com>
2020-02-06 01:16:36 +0300

    Implemented marriage

--- src/marriage.js
+++ src/marriage.js
@@ -8,15 +8,149 @@
  * @property {string[]} friends Список имён друзей
  */
 
+function createExtension(baseClass, constructor, prototype) {
+  constructor.prototype = prototype;
+  Object.setPrototypeOf(constructor.prototype, baseClass.prototype);
+  Object.defineProperty(constructor.prototype, 'constructor', {
+    value: constructor,
+    enumerable: false
+  });
+
+  return constructor;
+}
+
+/**
+ * Фильтр друзей
+ * @constructor
+ */
+const Filter = createExtension(Object, function() {}, {
+  test() {
+    return true;
+  }
+});
+
+/**
+ * Фильтр друзей-парней
+ * @extends Filter
+ * @constructor
+ */
+const MaleFilter = createExtension(Filter, function() {}, {
+  test(friend) {
+    return friend.gender === 'male';
+  }
+});
+
+/**
+ * Фильтр друзей-девушек
+ * @extends Filter
+ * @constructor
+ */
+const FemaleFilter = createExtension(Filter, function() {}, {
+  test(friend) {
+    return friend.gender === 'female';
+  }
+});
+
 /**
  * Итератор по друзьям
  * @constructor
  * @param {Friend[]} friends Список друзей
  * @param {Filter} filter Фильтр друзей
  */
-function Iterator(friends, filter) {
-  console.info(friends, filter);
-}
+const Iterator = createExtension(
+  Object,
+  function(friends, filter) {
+    if (!(filter instanceof Filter)) {
+      throw new TypeError("Argument 'filter' expected to be a Filter instance.");
+    }
+
+    let availableAndFilteredGuestCount = 0;
+    const possibleGuests = [];
+    const possibleGuestIndexes = [];
+
+    for (const friend of friends) {
+      const possibleGuest = {
+        person: friend,
+        available: friend.best,
+        filtered: filter.test(friend),
+        used: false,
+        ready: friend.best
+      };
+
+      possibleGuests.push(possibleGuest);
+      if (possibleGuest.available && possibleGuest.filtered) {
+        availableAndFilteredGuestCount++;
+      }
+    }
+
+    possibleGuests.sort((a, b) => a.person.name.localeCompare(b.person.name));
+    for (let i = 0; i < possibleGuests.length; i++) {
+      possibleGuestIndexes[possibleGuests[i].person.name] = i;
+    }
+
+    this._availableAndFilteredGuestCount = availableAndFilteredGuestCount;
+    this._possibleGuestIndexes = possibleGuestIndexes;
+    this._possibleGuests = possibleGuests;
+    this._index = 0;
+  },
+  {
+    _levelUp() {
+      const count = this._possibleGuests.length;
+      for (let i = 0; i < count; i++) {
+        const possibleGuest = this._possibleGuests[i];
+
+        if (possibleGuest.available) {
+          this._possibleGuests[i].ready = true;
+        }
+      }
+
+      this._index = 0;
+    },
+    next() {
+      let guest = undefined;
+
+      while (!this.done()) {
+        for (this._index = 0; this._index < this._possibleGuests.length; this._index++) {
+          const possibleGuest = this._possibleGuests[this._index];
+
+          if (guest) {
+            return guest;
+          }
+
+          if (possibleGuest.ready) {
+            for (const friend of possibleGuest.person.friends) {
+              const possibleGuestIndex = this._possibleGuestIndexes[friend];
+              const newGuest = this._possibleGuests[possibleGuestIndex];
+
+              if (!newGuest.ready && !newGuest.available && !newGuest.used) {
+                this._possibleGuests[possibleGuestIndex].available = true;
+
+                if (newGuest.filtered) {
+                  this._availableAndFilteredGuestCount++;
+                }
+              }
+            }
+
+            if (possibleGuest.filtered) {
+              this._possibleGuests[this._index].used = true;
+              this._possibleGuests[this._index].available = false;
+              this._possibleGuests[this._index].ready = false;
+              this._availableAndFilteredGuestCount--;
+
+              guest = possibleGuest.person;
+            }
+          }
+        }
+        this._levelUp();
+      }
+
+      return guest;
+    },
+    done() {
+      return this._availableAndFilteredGuestCount === 0;
+    }
+  }
+);
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -26,35 +160,24 @@ function Iterator(friends, filter) {
  * @param {Filter} filter Фильтр друзей
  * @param {Number} maxLevel Максимальный круг друзей
  */
-function LimitedIterator(friends, filter, maxLevel) {
-  console.info(friends, filter, maxLevel);
-}
+const LimitedIterator = createExtension(
+  Iterator,
+  function(friends, filter, maxLevel) {
+    Iterator.call(this, friends, filter);
 
-/**
- * Фильтр друзей
- * @constructor
- */
-function Filter() {
-  console.info('Filter');
-}
-
-/**
- * Фильтр друзей-парней
- * @extends Filter
- * @constructor
- */
-function MaleFilter() {
-  console.info('MaleFilter');
-}
-
-/**
- * Фильтр друзей-девушек
- * @extends Filter
- * @constructor
- */
-function FemaleFilter() {
-  console.info('FemaleFilter');
-}
+    this.level = 1;
+    this.maxLevel = maxLevel;
+  },
+  {
+    _levelUp() {
+      Iterator.prototype._levelUp.call(this);
+      this.level++;
+    },
+    done() {
+      return Iterator.prototype.done.call(this) || this.level > this.maxLevel;
+    }
+  }
+);
 
 module.exports = {
   Iterator,
